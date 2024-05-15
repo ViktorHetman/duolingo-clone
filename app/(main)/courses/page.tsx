f84c0b9163480df469a016ea366a294ad9474d9a
Viktor <vitya.getman.79@gmail.com>
2024-05-15 12:06:12 +0200

    courses page

---
+++ app/(main)/courses/page.tsx
@@ -0,0 +1,20 @@
+import { List } from './list'
+
+import { getCourses } from '@/db/queries'
+
+type Props = {}
+
+const CoursesPage: React.FC<Props> = async () => {
+  const courses = await getCourses()
+
+  return (
+    <div className='h-full max-w-[912px] px-3 mx-auto'>
+      <h1 className='text-2xl font-bold text-neutral-700'>
+        Language course title
+      </h1>
+      <List courses={courses} activeCourseId={1} />
+    </div>
+  )
+}
+
+export default CoursesPage
