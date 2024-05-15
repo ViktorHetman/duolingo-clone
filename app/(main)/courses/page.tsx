927e660860fff7fb989bc3e5a5c970c41d6ff564
Viktor <vitya.getman.79@gmail.com>
2024-05-15 13:02:19 +0200

    userProgress schema + redirecting to all courses page in case if there is no active course selected

--- app/(main)/courses/page.tsx
+++ app/(main)/courses/page.tsx
@@ -1,18 +1,24 @@
 import { List } from './list'
 
-import { getCourses } from '@/db/queries'
+import { getCourses, getUserProgress } from '@/db/queries'
 
 type Props = {}
 
 const CoursesPage: React.FC<Props> = async () => {
-  const courses = await getCourses()
+  const coursesPromise = getCourses()
+  const userProgressPromise = getUserProgress()
+
+  const [courses, userProgress] = await Promise.all([
+    coursesPromise,
+    userProgressPromise
+  ])
 
   return (
     <div className='h-full max-w-[912px] px-3 mx-auto'>
       <h1 className='text-2xl font-bold text-neutral-700'>
         Language course title
       </h1>
-      <List courses={courses} activeCourseId={1} />
+      <List courses={courses} activeCourseId={userProgress?.activeCourseId} />
     </div>
   )
 }
